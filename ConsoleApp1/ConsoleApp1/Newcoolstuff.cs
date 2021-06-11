af110605d307d0fc9bf1431eb29fc383de77494e
nix3220 <groovetablet@gmail.com>
2021-06-11 10:01:56 -0400

    New thigns added

---
+++ ConsoleApp1/ConsoleApp1/Newcoolstuff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Newcoolstuff
+    {
+        public void Cool()
+        {
+            //Do Cool Stuff Here
+        }
+    }
+}
